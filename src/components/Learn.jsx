881b5b47e1bf40bdbf63ab39056d9e8fa3c2deba
Bastos974 <sebastien.lemoine974@gmail.com>
2023-03-31 14:58:01 +0400

    🔥  liste de nom afficher par map

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react"
-import dataFruits from "../data/dataFruits"
-
+import { useState } from "react";
+import dataFruits from "../data/dataFruits";
 
 export default function Learn() {
+  //etat donnée
+  const [fruits, setfruits] = useState(dataFruits);
 
-//etat donnée
-const [fruits, setfruits] = useState(dataFruits)
+  //comportement
+  console.log(fruits);
 
-//comportement
-console.log(fruits)
+  //Affichage
 
-//Affichage
-
-
-
-
+  //{fruits.map( (item) =>{<div>bingo</div>} )}
 
   return (
-    <div>{fruits[0].nom}</div>
-  )
+    <div>
+      <div>{fruits[2].nom}</div>
+
+      {fruits.map((item) => (
+        <div>{item.nom}</div>
+      ))}
+    </div>
+  );
 }
